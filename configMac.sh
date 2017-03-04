3f91a70f24ed166e558a774efc0afad3869900fc
Raphael Kim <raphkay@Raphaelui-MacBook-Pro.local>
2017-03-04 13:51:18 +0900

    updating configMac.sh for using llvm-g** compiler in Mac OS.

--- configMac.sh
+++ configMac.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 # ====================================================================
-# This configMac.sh made for Mac OS X or above with HPC gcc.
+# This configMac.sh made for Mac OS X cocoa.
 # ===================================================================
+
+export CC="llvm-gcc"
+export CXX="llvm-g++"
+# export CFLAGS="-static -static-libstdc++ -static-libgcc"
+# export CXXFLAGS=${CFLAGS}
 
 ./configure  --with-abiversion=10304
