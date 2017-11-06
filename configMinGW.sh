72d06452b02e5e0a4c145c38ad1ba88c81fdc37b
Raphael Kim <rageworx@gmail.com>
2017-11-06 11:06:00 +0900

    Adding configure shell script for MinGW-W64

--- configMinGW.sh
+++ configMinGW.sh
@@ -5,6 +5,6 @@
 #   This configure shell script helps doing configure for MinGW-W64.
 
 EFLAGS="--enable-localjpeg --enable-localzlib --enable-localpng"
-AFLAGS="--width-abiversion=10304"
+AFLAGS="--with-abiversion=10304"
 
 ./configure ${EFLAGS} ${AFLAGS}
