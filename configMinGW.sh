f4b6242c431c1e36f7d4df6cfcb6eea260b17ac6
Raphael Kim <rageworx@gmail.com>
2018-09-09 01:31:16 +0900

    updating configMinGW.sh for MinGW-W64

--- configMinGW.sh
+++ configMinGW.sh
@@ -6,5 +6,5 @@
 
 EFLAGS="--enable-localjpeg --enable-localzlib --enable-localpng"
 AFLAGS="--with-abiversion=10304"
-
-./configure ${EFLAGS} ${AFLAGS}
+PREF="--prefix=/mingw --build=x86_64-w64-mingw32"
+./configure ${PREF} ${EFLAGS} ${AFLAGS}
