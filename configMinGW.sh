3ee0e6c5b154fe7c217aa27b92151df713f33fff
Raphael Kim <rageworx@gmail.com>
2019-03-02 21:39:56 +0900

    MinGW configuration shell for ABI 1.3.5

--- configMinGW.sh
+++ configMinGW.sh
@@ -5,6 +5,6 @@
 #   This configure shell script helps doing configure for MinGW-W64.
 
 EFLAGS="--enable-localjpeg --enable-localzlib --enable-localpng"
-AFLAGS="--with-abiversion=10304"
+AFLAGS="--with-abiversion=10305"
 BSYS="--build=x86_64-w64-mingw32"
 ./configure ${BSYS} ${EFLAGS} ${AFLAGS}
