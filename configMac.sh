a05aa6cbf81dc7b3dd01e96178a40e769c000131
Rapahael Kim <rageworx@gmail.com>
2021-04-11 19:05:54 +0900

    configMac.sh automatically sens Darwin kernel version, and settig up for universal binary option for Big Sur

--- configMac.sh
+++ configMac.sh
@@ -7,5 +7,24 @@
 
 COMPILER="CC=llvm-gcc CXX=llvm-g++"
 FEATURES="--enable-localjpeg  --enable-localzlib --enable-localpng"
+FLTKABI="--with-abiversion=10305"
 
-./configure ${FEATURES} ${COMPILER} --with-abiversion=10305
+KVER_0=`uname -r | cut -d . -f1`
+KVER_1=`uname -r | cut -d . -f2`
+KVER_2=`uname -r | cut -d . -f3`
+KVER_A=`uname -m`
+
+echo "Darwin Kernel version identified ${KVER_0}.${KVER_1}.${KVER_2}, architecture : ${KVER_A}"
+
+# Sens kernel version ...
+# -----------------------
+# ~ 18.x : Mojave
+#   19.x : Catalina
+#   20.x : Big Sur   ( Supporting arm64 archicture )
+
+if [ ${KVER_0} -gt 19 ];then
+    echo "Setting for universal binary for Big Sur"
+    export ARCHFLAGS="-arch x86_64 -arch arm64"  
+fi
+
+echo ./configure ${FEATURES} ${COMPILER} ${FLTKABI}
